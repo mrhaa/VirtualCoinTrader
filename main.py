3eedc481685a552d1a93fff7362e76b46ac1643f
ryusangjin <trader.sjryu@gmail.com>
2021-03-29 19:46:26 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 #_*_ coding: utf-8 _*_
 
 import BatchManager
+import pyupbit
 
 if __name__ == '__main__':
 
@@ -13,25 +14,32 @@ if __name__ == '__main__':
 
     batch = BatchManager.BatchManager(PRINT_BALANCE_STATUS_LOG, PRINT_TRADABLE_MARKET_LOG, PRINT_DATA_LOG, PROCEDURE_ERR_LOG, API_ERR_LOG)
 
+    if 0:
+        TEST_LOGIC = False
+        batch.test(TEST_LOGIC)
 
-    TEST_LOGIC = False
-    batch.test(TEST_LOGIC)
 
 
-    batch.make_db_for_learner()
+    if 0:
+        # 프로시저 작업 활성화 단계 설정(전 단계가 True인 경우 다음 단계에가 활성화될 수 있음)
+        READ_BALANCE = True  # 1. 비트업에서 계좌 정보를 읽음
+        READ_MARKET = True  # 2. 비트업에서 거래 가능한 종목 리스트업
+        READ_DATA = True  # 3. 비트업에서 시계열 데이터 읽음
+        ANALYZE_DATA = True  # 4. 시그널 생성
+        TRADE_COIN = True  # 5. 시그널에 맞춰 매매
 
-    # 프로시저 작업 활성화 단계 설정(전 단계가 True인 경우 다음 단계에가 활성화될 수 있음)
-    READ_BALANCE = True  # 1. 비트업에서 계좌 정보를 읽음
-    READ_MARKET = True  # 2. 비트업에서 거래 가능한 종목 리스트업
-    READ_DATA = True  # 3. 비트업에서 시계열 데이터 읽음
-    ANALYZE_DATA = True  # 4. 시그널 생성
-    TRADE_COIN = False  # 5. 시그널에 맞춰 매매
+        # 옵션한 기능 활성화 설정
+        EMPTY_ALL_POSITION = True  # 모든 포지션 매도 후 프로그램 종료
+        CALL_TERM_APPLY = False  # API 오류 빈도에 따라 루프 주기를 자동 조절
 
-    # 옵션한 기능 활성화 설정
-    EMPTY_ALL_POSITION = False  # 모든 포지션 매도 후 프로그램 종료
-    CALL_TERM_APPLY = False  # API 오류 빈도에 따라 루프 주기를 자동 조절
+        batch.loop_procedures(READ_BALANCE, READ_MARKET, READ_DATA, ANALYZE_DATA, TRADE_COIN
+                              , EMPTY_ALL_POSITION, CALL_TERM_APPLY)
 
-    batch.loop_procedures(READ_BALANCE, READ_MARKET, READ_DATA, ANALYZE_DATA, TRADE_COIN
-                          , EMPTY_ALL_POSITION, CALL_TERM_APPLY)
+    if 1:
+        # 프로시저 작업 활성화 단계 설정(전 단계가 True인 경우 다음 단계에가 활성화될 수 있음)
+        READ_MARKET = True  # 1. 비트업에서 거래 가능한 종목 리스트업
+        READ_DATA = True  # 2. 비트업에서 시계열 데이터 읽음
+
+        batch.make_db_for_learner(READ_MARKET, READ_DATA)
 
 
