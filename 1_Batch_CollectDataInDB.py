4be782e06e89967a3904022ba9110242d0c865a4
ryusangjin <trader.sjryu@gmail.com>
2021-04-21 18:40:52 +0900

    Update 1_Batch_CollectDataInDB.py

--- 1_Batch_CollectDataInDB.py
+++ 1_Batch_CollectDataInDB.py
@@ -9,8 +9,10 @@ if __name__ == '__main__':
     PRINT_DATA_LOG = False
     PROCEDURE_ERR_LOG = True  # 메인 프로시저 동작 시 오류
     API_ERR_LOG = False  # 비스업 API 호출 시 오류
+    SIMULATION = False  # 시뮬레이션 여부
 
-    batch = BatchManager.BatchManager(PRINT_BALANCE_STATUS_LOG, PRINT_TRADABLE_MARKET_LOG, PRINT_DATA_LOG, PROCEDURE_ERR_LOG, API_ERR_LOG)
+    batch = BatchManager.BatchManager(PRINT_BALANCE_STATUS_LOG=PRINT_BALANCE_STATUS_LOG, PRINT_TRADABLE_MARKET_LOG=PRINT_TRADABLE_MARKET_LOG, PRINT_DATA_LOG=PRINT_DATA_LOG
+                                      , PROCEDURE_ERR_LOG=PROCEDURE_ERR_LOG, API_ERR_LOG=API_ERR_LOG, SIMULATION=SIMULATION)
 
     # 프로시저 작업 활성화 단계 설정(전 단계가 True인 경우 다음 단계에가 활성화될 수 있음)
     READ_MARKET = True  # 1. 비트업에서 거래 가능한 종목 리스트업
